f7f18b9439e45587dd4f54888e4280853978d435
Tez613 <tevishadley@gmail.com>
2022-10-27 21:02:45 -0600

    Working ship movement

--- Program.cs
+++ Program.cs
@@ -1,2 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int exit = 0;
+int playerPosition = 1;
+while (exit < 1)
+{
+    var keyInfo = Console.ReadKey();
+    switch (keyInfo.Key)
+    {
+        case ConsoleKey.LeftArrow:
+            //rotates ship to the left
+            playerPosition++;
+            break;
+        case ConsoleKey.RightArrow:
+            //rotates ship to the right
+            playerPosition--;
+            break;
+        case ConsoleKey.UpArrow:
+            //Advances the game one frame
+            break;
+        case ConsoleKey.Spacebar:
+            //Shoots the weapon
+            break;
+    }
+    switch (playerPosition)
+    {
+        case 1:
+            Console.Clear();
+            Console.Write(@"/\");
+            break;
+        case 2:
+            Console.Clear();
+            Console.Write(@"|\");
+            break;
+        case 3:
+            Console.Clear();
+            Console.Write("<");
+            break;
+        case 4:
+            Console.Clear();
+            Console.Write("|/");
+            break;
+        case 5:
+            Console.Clear();
+            Console.Write(@"\/");
+            break;
+        case 6:
+            Console.Clear();
+            Console.Write(@"\|");
+            break;
+        case 7:
+            Console.Clear();
+            Console.Write(">");
+            break;
+        case 8:
+            Console.Clear();
+            Console.Write("/|");
+            break;
+    }
+    while (playerPosition > 8)
+    {
+        playerPosition = 1;
+    }
+    while (playerPosition < 1)
+    {
+        playerPosition = 8;
+    }
+}
+
+
