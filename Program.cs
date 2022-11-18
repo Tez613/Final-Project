3f9838f8c139b7f764e903243a9dac2c9cacc505
Tez613 <tevishadley@gmail.com>
2022-11-17 18:40:37 -0700

    mostly working

--- Program.cs
+++ Program.cs
@@ -1,23 +1,23 @@
-﻿char [,] grid = new char [9,9];
+﻿char[,] grid = new char[9, 9];
 void draw()
 {
 
-for (int row = 0; row <grid.GetLength(1); row++)
-{
-    for(int column = 0; column <grid.GetLength(1);column++)
-        Console.Write(grid[row, column]+ "0");
+    for (int row = 0; row < grid.GetLength(1); row++)
+    {
+        for (int column = 0; column < grid.GetLength(1); column++)
+            Console.Write(grid[row, column] + "  ");
 
-    Console.WriteLine();
+        Console.WriteLine();
+    }
 }
-}
- 
+
 
 int exit = 0;
 int playerPosition = 1;
 Console.OutputEncoding = System.Text.Encoding.Unicode;
 while (exit < 1)
 {
-Console.Clear();
+
     var keyInfo = Console.ReadKey();
     switch (keyInfo.Key)
     {
@@ -33,55 +33,59 @@ Console.Clear();
             //Advances the game one frame
             break;
         case ConsoleKey.Spacebar:
+            Console.Beep(500, 250);
             //Shoots the weapon
+            break;
+        case ConsoleKey.Escape:
+            //exits the game
+            exit = 1;
             break;
     }
     switch (playerPosition)
     {
-         case 0:
-             Console.Clear();
-            
-            grid[5,5]= '⯅';
+        case 0:
+            Console.Clear();
+            grid[5, 5] = '⯅';
             break;
         case 1:
-              Console.Clear();
-            grid[5,5]= '⯅';
+            Console.Clear();
+            grid[5, 5] = '⯅';
             break;
         case 2:
-             Console.Clear();
-            grid[5,5]='◤';
+            Console.Clear();
+            grid[5, 5] = '◤';
             break;
         case 3:
-             Console.Clear();
-            grid[5,5]= '⯇';
+            Console.Clear();
+            grid[5, 5] = '⯇';
             break;
         case 4:
-             Console.Clear();
-            grid[5,5]= '◣';
+            Console.Clear();
+            grid[5, 5] = '◣';
             break;
         case 5:
-             Console.Clear();
-            grid[5,5]= '⯆';
+            Console.Clear();
+            grid[5, 5] = '⯆';
             break;
         case 6:
-             Console.Clear();
-            grid[5,5]= '◢';
+            Console.Clear();
+            grid[5, 5] = '◢';
             break;
         case 7:
-             Console.Clear();
-            grid[5,5]= '⯈';
+            Console.Clear();
+            grid[5, 5] = '⯈';
             break;
         case 8:
-             Console.Clear();
-            grid[5,5]= '◥';
+            Console.Clear();
+            grid[5, 5] = '◥';
             break;
         case 9:
-             Console.Clear();
-            grid[5,5]= '⯅';
+            Console.Clear();
+            grid[5, 5] = '⯅';
             break;
 
     }
-    //cycle through player position
+    //cycles through the player position
     if (playerPosition == 9)
     {
         playerPosition = 1;
