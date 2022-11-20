bc4464bfe5aae17dc5f96d521bef34b20201fe9d
Tez613 <tevishadley@gmail.com>
2022-11-20 16:55:22 -0700

    ship can strafe now

--- Program.cs
+++ Program.cs
@@ -10,8 +10,8 @@ void draw()
         Console.WriteLine();
     }
 }
-
-
+int xAxis = 5;
+int yAxis = 5;
 int exit = 0;
 int playerPosition = 1;
 Console.OutputEncoding = System.Text.Encoding.Unicode;
@@ -24,10 +24,18 @@ while (exit < 1)
         case ConsoleKey.LeftArrow:
             //rotates ship to the left
             playerPosition++;
+            if (playerPosition == 9)
+            {
+                playerPosition = 1;
+            }
             break;
         case ConsoleKey.RightArrow:
             //rotates ship to the right
             playerPosition--;
+            if (playerPosition == 1)
+            {
+                playerPosition = 9;
+            }
             break;
         case ConsoleKey.UpArrow:
             //Advances the game one frame
@@ -40,59 +48,78 @@ while (exit < 1)
             //exits the game
             exit = 1;
             break;
+        case ConsoleKey.A:
+            //Move the ship left
+            xAxis--;
+            if (xAxis == -1)
+            {
+                xAxis = 8;
+            }
+            break;
+        case ConsoleKey.D:
+            //Move the ship right
+            xAxis++;
+            if (xAxis == 9)
+            {
+                xAxis = 0;
+            }
+            break;
+        case ConsoleKey.W:
+            //Move the ship up
+            yAxis--;
+            if (yAxis == -1)
+            {
+                yAxis = 8;
+            }
+            break;
+        case ConsoleKey.S:
+            //Move the ship down
+            yAxis++;
+            if (yAxis == 9)
+            {
+                yAxis = 0;
+            }
+            break;
     }
     switch (playerPosition)
     {
-        case 0:
-            Console.Clear();
-            grid[5, 5] = '⯅';
-            break;
         case 1:
             Console.Clear();
-            grid[5, 5] = '⯅';
+            grid[yAxis, xAxis] = '⯅';
             break;
         case 2:
             Console.Clear();
-            grid[5, 5] = '◤';
+            grid[yAxis, xAxis] = '◤';
             break;
         case 3:
             Console.Clear();
-            grid[5, 5] = '⯇';
+            grid[yAxis, xAxis] = '⯇';
             break;
         case 4:
             Console.Clear();
-            grid[5, 5] = '◣';
+            grid[yAxis, xAxis] = '◣';
             break;
         case 5:
             Console.Clear();
-            grid[5, 5] = '⯆';
+            grid[yAxis, xAxis] = '⯆';
             break;
         case 6:
             Console.Clear();
-            grid[5, 5] = '◢';
+            grid[yAxis, xAxis] = '◢';
             break;
         case 7:
             Console.Clear();
-            grid[5, 5] = '⯈';
+            grid[yAxis, xAxis] = '⯈';
             break;
         case 8:
             Console.Clear();
-            grid[5, 5] = '◥';
+            grid[yAxis, xAxis] = '◥';
             break;
         case 9:
             Console.Clear();
-            grid[5, 5] = '⯅';
+            grid[yAxis, xAxis] = '⯅';
             break;
 
-    }
-    //cycles through the player position
-    if (playerPosition == 9)
-    {
-        playerPosition = 1;
-    }
-    else if (playerPosition == 1)
-    {
-        playerPosition = 9;
     }
     draw();
 }
